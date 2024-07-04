cacabf2ce2a4cccc09f47604d87ec6f148cf2a0f
NiMarian <nicuta.marian12@gmail.com>
2024-07-05 01:03:35 +0300

    n

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import './Navbar.css'
 import navlogo from '../../assets/nav-logo2.png'
-import navProfile from '../../assets/nav-profile.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={navlogo} alt="Logo" className="nav-logo" />
-        <img src={navProfile} className="nav-profile" alt="Profile" />
     </div>
   )
 }
