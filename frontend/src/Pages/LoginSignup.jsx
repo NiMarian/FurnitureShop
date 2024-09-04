880e76c221d25c60f3881f9d30717465ed84fe90
NiMarian <nicuta.marian12@gmail.com>
2024-09-04 16:01:32 +0300

    s

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -75,7 +75,7 @@ const LoginSignup = () => {
     if (responseData.success) {
       alert("Înregistrarea a avut loc cu succes! Spor la cumpărături!");
       localStorage.setItem('auth-token', responseData.token);
-      window.location.href = 'https://furnitureshop-backend.onrender.com/';
+      window.location.href = 'https://furnitureshop-frontend.onrender.com/';
     } else {
       alert(responseData.errors);
     }
